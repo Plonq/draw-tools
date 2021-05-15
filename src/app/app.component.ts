cdef952f16f0455ec13bc347af0be30522a8fa4e
Huon Imberger <huon@plonq.org>
2021-05-15 11:02:41 +1000

    Some bablyon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Engine} from "@babylonjs/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements AfterViewInit{
+  private engine: Engine;
+  @ViewChild("canvas", {static: true}) private canvas: ElementRef<HTMLCanvasElement>;
+
+  ngAfterViewInit() {
+    this.engine = new Engine(this.canvas.nativeElement, true, {
+      preserveDrawingBuffer: true,
+      stencil: true,
+    });
+
+    window.addEventListener('resize', () => {
+      this.engine.resize();
+    });
+  }
 }
